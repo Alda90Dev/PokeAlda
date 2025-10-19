ea0ed9e13078a2c6c5c906426f2bd31afd9b7e74
Aldair Carrillo <alda90dev@gmail.com>
2025-10-18 17:12:56 -0700

    First Commit

---
+++ PokeAlda/PokeAldaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAldaApp.swift
+//  PokeAlda
+//
+//  Created by Aldair Carrillo on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeAldaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
